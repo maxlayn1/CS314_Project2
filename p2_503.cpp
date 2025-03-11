a02d5f73f0a1df6024bd9710a40e2940aa7d91ea
maxlayn1 <maxlayn1@gmail.com>
2025-03-11 10:04:36 -0500

    added suggested code skeleton

--- p2_503.cpp
+++ p2_503.cpp
@@ -1,1 +1,33 @@
-// Maxim Tikhonov 503 Section 001 @mtikhon@siue.edu
+/* ********************************************************** *
+ * CS314 Project #2 solution                                  *
+ *                                                            *
+ * Your last-three:                                           *
+ * Your course section #:                                     *
+ *                                                            *
+ * Spring 2025                                                *
+ *                                                            *
+ * You can "copy & paste" this to your source code file       *
+ *                                                            *
+ * ********************************************************** */
+
+#define NUM_REPEAT                  50   // each boiler-man repeats
+
+#define BATHER_TIME_01_A        300000   // 300000us = 0.3 seconds
+#define BATHER_TIME_01_B        800000   // 800000us = 0.8 seconds
+
+#define BATHER_TIME_02_A        300000   // 300000us = 0.3 seconds
+#define BATHER_TIME_02_B        800000   // 800000us = 0.8 seconds
+
+#define BATHER_TIME_03_A        300000   // 300000us = 0.3 seconds
+#define BATHER_TIME_03_B        800000   // 800000us = 0.8 seconds
+
+#define BOLIERMAN_TIME_01_A    1200000   // 1200000us = 1.2 seconds
+#define BOLIERMAN_TIME_01_B    1600000   // 1600000us = 1.6 seconds
+
+#define BOLIERMAN_TIME_02_A    1200000   // 1200000us = 1.2 seconds
+#define BOLIERMAN_TIME_02_B    1600000   // 1600000us = 1.6 seconds
+
+#define SAFEGUARD_TIME_A       1200000   // 1200000us = 1.2 seconds
+#define SAFEGUARD_TIME_B       1600000   // 1600000us = 1.6 seconds
+
+#include <iostream>
